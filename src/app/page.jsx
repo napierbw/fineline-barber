6f90fdfa8e4762b18a0b0c76ae226c5c122b62e1
Benjamin Napier <napierbw51@gmail.com>
2024-12-10 00:26:14 -0500

    add aos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,22 @@
+"use client"
+
 import NavBar from "./components/NavBar/NavBar"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Intro from "./components/Intro"
 import Services from "./components/Services"
 import Location from "./components/Location"
+import AOS from "aos";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration
+      once: true, // Animation happens only once
+    });
+  }, []);
+
   return <div>
     <NavBar/>
     <Hero/>
